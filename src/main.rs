94f12c34e9f39b05d6b1a16e975f10b09611362d
Michael Houmann <mhoumann87@gmail.com>
2025-09-11 12:41:41 +0200

    Add match in function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Import the libraries you need
-use std::io;
+use std::{cmp::Ordering, io};
 // We use the Rand library (crate) to generate a random number
 use rand::prelude::*;
 
@@ -37,10 +37,35 @@ fn main() {
     // We need to change the string to a number for it to work
     let guess: u32 = guess.trim().parse().expect("Error with parse");
 
-    // We can use an if expression (much like an if statement in other languages)
-    if guess == correct {
-        println!("You guessed the correct number {}", guess);
+    // We can use an if / else expression (much like an if / else statement in other languages)
+    /* if guess < correct {
+        println!("You've guessed too low");
+    } else if guess > correct {
+        println!("You've guessed to high");
     } else {
-        println!("You guessed wrong. The correct number was {}", correct);
-    }
+        println!("You guessed the correct number");
+    }*/
+
+    // A "more correct way" to do this is
+    /* let message = if guess > correct {
+        String::from("You've guessed to high")
+    } else if guess < correct {
+        String::from("You've guessed to low")
+    } else {
+        String::from("You guessed correct")
+    };
+    // Notice that there are no semi colons after the strings, it is after the total expression
+
+    // And then we have to print the correct string
+    println!("{}", message); */
+
+    // We can also use a match statement in Rust to run the game
+    let message = match guess.cmp(&correct) {
+        Ordering::Greater => "You guessed to high",
+        Ordering::Less => "You guessed to low",
+        Ordering::Equal => "You are correct",
+    };
+
+    // And print the message
+    println!("{message}");
 }
